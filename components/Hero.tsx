228e43296c7c45fc80207d1fca12f5adb34a19de
TheLeafAgency <media@theleafagency.org>
2026-09-16 13:06:07 -0400

    Move portfolio button above divider

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -66,7 +66,7 @@ export default function Hero() {
           position: "absolute",
           left: 0,
           right: 0,
-          bottom: "24px",
+          bottom: "110px",
           zIndex: 3,
           display: "flex",
           justifyContent: "center",
