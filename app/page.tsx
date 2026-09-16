1cec370184048d6954ff73cdc27be0fe149fd420
TheLeafAgency <media@theleafagency.org>
2026-09-16 12:28:23 -0400

    Place services section on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import ServicesSection from "@/components/ServicesSection";
 import ServiceCards from "@/components/ServiceCards";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main id="top">
       <Navbar />
       <Hero />
+      <ServicesSection />
       <ServiceCards />
     </main>
   );
