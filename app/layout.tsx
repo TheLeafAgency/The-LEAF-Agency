d94da485dfdaf7579f0fd17d1cc5ea01ad42366f
TheLeafAgency <media@theleafagency.org>
2026-09-16 12:49:48 -0400

    Start page at top on refresh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import ScrollToTopOnLoad from "@/components/ScrollToTopOnLoad";
 
 export const metadata: Metadata = {
   title: "LEAF Agency",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ScrollToTopOnLoad />
+        {children}
+      </body>
     </html>
   );
 }
