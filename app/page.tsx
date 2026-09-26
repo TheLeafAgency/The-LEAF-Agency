8384855cd47bcb6387bc04ded2146e727b00bac5
TheLeafAgency <media@theleafagency.org>
2026-09-26 13:29:08 -0400

    Add botanical layer to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
 import ServiceCards from "@/components/ServiceCards";
 import ContactSection from "@/components/ContactSection";
+import BotanicalGrowth from "@/components/BotanicalGrowth";
 
 export default function Home() {
   return (
-    <main id="top">
+    <main id="top" className="leaf-botanical-page">
+      <BotanicalGrowth />
       <Navbar />
       <Hero />
       <AboutSection />
